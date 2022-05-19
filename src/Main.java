475e1e71d5cd8f3779e3a08fe017d9d4926200a7
mani123 <worldmandiareload@gmail.com>
2022-05-19 18:16:28 +0200

    loll

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,10 @@ public class Main {
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
+        System.out.println("Task - 1");
         new Task1(hashMap);
+        System.out.println("Task - 2");
         new Task2(scanner, hashMap);
+        System.out.println("Task - 3");
     }
 }
